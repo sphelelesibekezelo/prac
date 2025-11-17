0922593a7e186edb5ccee165d5ab0c8ac5a2714d
sphelelesibekezelo <sphelelesibekezelo@gmail.com>
2025-11-17 11:02:02 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+ import express from 'express';
+ import users from './user.js';
+
+ const app = express();
+
+ app.get('/', (req, res) => {
+   res.send("Server is ready");
+ })
+
+ app.get('/api/users', (req, res) => {
+    res.send(users);
+  })
+
+ const port = process.env.PORT || 3000
+
+ app.listen(port, () => {
+    console.log(`Server at http://localhost:${port}`)
+    })
+ 
